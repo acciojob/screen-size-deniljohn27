315aabb7c2c9c7bf96ee7cf51bda3af600d757d1
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 21:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let width=window.innerWidth;
+let height=window.innerHeight;
+let ele=document.createElement("h1");
+ele.innerText=`Width: ${width} and Height: ${height}`;
+document.body.append(ele);
+
+window.onresize=function(){
+	ele.innerText=`Width: ${width} and Height: ${height}`
+};
+
