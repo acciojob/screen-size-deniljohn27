040872b7260b2638f14622daa27aef1f0adb6ae5
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 21:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 let ele=document.createElement("h1");
+let div=document.getElementById("sizeInfo");
 ele.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
-document.body.append(ele);
+div.append(ele);
 
 window.onresize=function(){
 	ele.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
